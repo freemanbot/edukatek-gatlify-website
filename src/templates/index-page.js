8f08f2e9a10dcabadf0380c492cbb36fb6429b48
Aminerj <aminerj@protonmail.com>
2020-04-17 13:43:08 +0200

    working on the new, simplified version of the website

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -20,160 +20,176 @@ export const IndexPageTemplate = ({
   // testimonials,
   // fullImage
 }) => (
-  <div>
-    <div
-      className="full-width-image margin-top-0"
-      style={{
-        backgroundImage: `url(${
-          !!image.childImageSharp ? image.childImageSharp.fluid.src : image
-        })`,
-        backgroundPosition: `top center`
-        // backgroundAttachment: `fixed`
-      }}
-    >
+    <div>
       <div
+        className="full-width-image margin-top-0"
         style={{
-          display: "flex",
-          height: "150px",
-          lineHeight: "1",
-          justifyContent: "space-around",
-          alignItems: "left",
-          flexDirection: "column"
+          backgroundImage: `url(${
+            !!image.childImageSharp ? image.childImageSharp.fluid.src : image
+            })`,
+          backgroundPosition: `top center`
+          // backgroundAttachment: `fixed`
         }}
       >
-        <h1
-          className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {title}
-        </h1>
-        <h3
-          className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
-          style={{
-            boxShadow:
-              "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
-            backgroundColor: "rgba(0, 0, 0, 0.7)",
-            color: "white",
-            lineHeight: "1",
-            padding: "0.25em"
-          }}
-        >
-          {subheading}
-        </h3>
-
         <div
-          className="content buttons is-centered"
           style={{
-            marginTop: "1em"
+            display: "flex",
+            height: "150px",
+            lineHeight: "1",
+            justifyContent: "space-around",
+            alignItems: "left",
+            flexDirection: "column"
           }}
         >
-          {/* <button class="button is-large is-primary is-outlined"> */}
-          <a
-            class="button is-primary is-light is-medium is-primary"
-            href="https://learn.edukatek.com/login/index.php"
-            target="_blank"
-            rel="noopener noreferrer"
+          <h1
+            className="has-text-weight-bold is-size-3-mobile is-size-2-tablet is-size-1-widescreen"
+            style={{
+              boxShadow:
+                "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+              color: "white",
+              lineHeight: "1",
+              padding: "0.25em"
+            }}
           >
-            Accéder à notre ENT
-          </a>
+            {title}
+          </h1>
+          <h3
+            className="has-text-weight-bold is-size-5-mobile is-size-5-tablet is-size-4-widescreen"
+            style={{
+              boxShadow:
+                "rgb(255, 68, 0) 0.5rem 0px 0px, rgb(255, 68, 0) -0.5rem 0px 0px",
+              backgroundColor: "rgba(0, 0, 0, 0.7)",
+              color: "white",
+              lineHeight: "1",
+              padding: "0.25em"
+            }}
+          >
+            {subheading}
+          </h3>
+          <div class="content buttons is-centered">
+            <div class="field buttons are-medium is-grouped is-centered"
+              style={{
+                padding: "0.8rem"
+              }}>
+              <p class="control">
+                <a class="button is-primary" href="#parents">
+                  Parents</a>
+              </p>
+              <p class="control">
+                <a class="button is-primary" href="#enseigants">Enseignants</a>
+              </p>
+              <p class="control">
+                <button class="button is-primary">Écoles</button>
+              </p>
+              {/* <p class="control">
+                <button class="button is-primary">FAQ</button>
+              </p> */}
+            </div>
+          </div>
+          {/* <div
+            className="content buttons is-centered"
+            style={{
+              marginTop: "1em"
+            }}
+          > 
+          <a
+              class="button is-primary is-light is-medium is-primary"
+              href="https://learn.edukatek.com/login/index.php"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Accéder à notre ENT
+          </a> */}
           {/* </button> */}
+          {/* </div> */}
         </div>
       </div>
-    </div>
-    <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <div className="content">
-                  <div className="tile">
-                    <h1 className="title">{mainpitch.title}</h1>
-                  </div>
-                  <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
-                  </div>
-                </div>
+      <section id="perents"
+        className="section section--gradient">
+        <div className="container">
+          <div className="content">
+            <div className="tile">
+              <h1 className="title">{mainpitch.title}</h1>
+            </div>
+            <div className="tile">
+              <h3 className="subtitle">{mainpitch.description}</h3>
+            </div>
 
-                <div className="columns">
-                  <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      {heading}
-                    </h3>
-                    <p>{description}</p>
-                  </div>
-                </div>
-                <Features gridItems={intro.blurbs} />
-                {/* <div className="columns">
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/products">
-                      See all products
-                    </Link>
-                  </div>
-                </div>
-                <div className="column is-12">
-                  <h3 className="has-text-weight-semibold is-size-2">
-                    Latest stories
-                  </h3>
-                  <BlogRoll />
-                  <div className="column is-12 has-text-centered">
-                    <Link className="btn" to="/blog">
-                      Read more
-                    </Link>
-                  </div>
-                </div> */}
+            <div className="columns">
+              <div className="column is-12">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  {heading}
+                </h3>
+                <p>{description}</p>
               </div>
             </div>
+            <Features gridItems={intro.blurbs} />
           </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-    <div
-      className="content buttons is-centered"
-      style={{
-        paddingBottom: "4em"
-      }}
-    >
-      {/* <button class="button is-large is-primary is-outlined"> */}
-      <a class="button is-primary is-large is-primary" href="/contact">
-        Demandez votre ENT
+      <section id="enseignants"
+        className="section section--gradient">
+        <div className="container">
+          <div className="content">
+            <div className="tile">
+              <h1 className="title">{mainpitch.title}</h1>
+            </div>
+            <div className="tile">
+              <h3 className="subtitle">{mainpitch.description}</h3>
+            </div>
+
+            <div className="columns">
+              <div className="column is-12">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  {heading}
+                </h3>
+                <p>{description}</p>
+              </div>
+            </div>
+            <Features gridItems={intro.blurbs} />
+          </div>
+        </div>
+      </section>
+
+      <section id="schools-section"
+        className="section section--gradient">
+        <div className="container">
+          <div className="content">
+            <div className="tile">
+              <h1 className="title">{mainpitch.title}</h1>
+            </div>
+            <div className="tile">
+              <h3 className="subtitle">{mainpitch.description}</h3>
+            </div>
+
+            <div className="columns">
+              <div className="column is-12">
+                <h3 className="has-text-weight-semibold is-size-2">
+                  {heading}
+                </h3>
+                <p>{description}</p>
+              </div>
+            </div>
+            <Features gridItems={intro.blurbs} />
+          </div>
+        </div>
+      </section>
+
+      <div
+        className="content buttons is-centered"
+        style={{
+          paddingBottom: "4em"
+        }}
+      >
+        <a class="button is-primary is-large is-primary" href="/contact">
+          Demandez votre ENT
       </a>
-      {/* </button> */}
-    </div>
-
-    {/* <section className="section section--gradient">
-      <div className="container">
-        <div className="section">
-          <div className="columns">
-            <div className="column is-10 is-offset-1">
-              <div className="content">
-                <Testimonials testimonials={testimonials} />
-                <div
-                  className="full-width-image-container"
-                  style={{
-                    backgroundImage: `url(${
-                      fullImage.childImageSharp
-                        ? fullImage.childImageSharp.fluid.src
-                        : fullImage
-                    })`
-                  }}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
       </div>
-    </section> */}
-  </div>
-);
+
+    </div>
+  );
 
 IndexPageTemplate.propTypes = {
   image: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
@@ -202,8 +218,8 @@ const IndexPage = ({ data }) => {
         mainpitch={frontmatter.mainpitch}
         description={frontmatter.description}
         intro={frontmatter.intro}
-        // testimonials={frontmatter.testimonials}
-        // fullImage={frontmatter.full_image}
+      // testimonials={frontmatter.testimonials}
+      // fullImage={frontmatter.full_image}
       />
     </Layout>
   );
